5340f2e9dbf128b2b79dfd41a7c97ef74a69793c
Cyborg-Squirrel <ashlybelka@gmail.com>
2026-05-12 22:33:58 -0500

    Code cleanup

--- src/main/kotlin/io/cyborgsquirrel/lighting/effects/LightEffectConstants.kt
+++ src/main/kotlin/io/cyborgsquirrel/lighting/effects/LightEffectConstants.kt
@@ -1,28 +1,26 @@
 package io.cyborgsquirrel.lighting.effects
 
-class LightEffectConstants {
-    companion object {
-        // Bouncing ball
-        const val EARTH_GRAVITY = -9.81
-        const val MOON_GRAVITY = -1.62
+object LightEffectConstants {
+    // Bouncing ball
+    const val EARTH_GRAVITY = -9.81
+    const val MOON_GRAVITY = -1.62
 
-        // Names
-        const val SPECTRUM_NAME = "Spectrum"
-        const val NIGHTRIDER_COLOR_FILL_NAME = "Nightrider Color Fill"
-        const val NIGHTRIDER_COMET_NAME = "Comet"
-        const val FLAME_EFFECT_NAME = "Flame"
-        const val BOUNCING_BALL_NAME = "Bouncing Ball"
-        const val WAVE_EFFECT_NAME = "Wave"
-        const val MARQUEE_EFFECT_NAME = "Marquee"
+    // Names
+    const val SPECTRUM_NAME = "Spectrum"
+    const val NIGHTRIDER_COLOR_FILL_NAME = "Nightrider Color Fill"
+    const val NIGHTRIDER_COMET_NAME = "Comet"
+    const val FLAME_EFFECT_NAME = "Flame"
+    const val BOUNCING_BALL_NAME = "Bouncing Ball"
+    const val WAVE_EFFECT_NAME = "Wave"
+    const val MARQUEE_EFFECT_NAME = "Marquee"
 
-        val allEffectNames = listOf(
-            SPECTRUM_NAME,
-            NIGHTRIDER_COLOR_FILL_NAME,
-            NIGHTRIDER_COMET_NAME,
-            FLAME_EFFECT_NAME,
-            BOUNCING_BALL_NAME,
-            WAVE_EFFECT_NAME,
-            MARQUEE_EFFECT_NAME
-        )
-    }
+    val allEffectNames = listOf(
+        SPECTRUM_NAME,
+        NIGHTRIDER_COLOR_FILL_NAME,
+        NIGHTRIDER_COMET_NAME,
+        FLAME_EFFECT_NAME,
+        BOUNCING_BALL_NAME,
+        WAVE_EFFECT_NAME,
+        MARQUEE_EFFECT_NAME
+    )
 }
