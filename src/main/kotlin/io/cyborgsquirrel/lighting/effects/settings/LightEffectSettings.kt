55f87ced682804ee3a7352b653e1f46c0e28f318
Cyborg-Squirrel <ashlybelka@gmail.com>
2026-05-13 19:10:21 -0500

    Fixed build

--- src/main/kotlin/io/cyborgsquirrel/lighting/effects/settings/LightEffectSettings.kt
+++ src/main/kotlin/io/cyborgsquirrel/lighting/effects/settings/LightEffectSettings.kt
@@ -3,5 +3,5 @@ package io.cyborgsquirrel.lighting.effects.settings
 import io.cyborgsquirrel.lighting.SettingsMetadata
 import io.cyborgsquirrel.lighting.HasMetadata
 
-sealed class LightEffectSettings(metadata: SettingsMetadata = SettingsMetadata()) :
-    HasMetadata(metadata)
+sealed class LightEffectSettings(override val metadata: SettingsMetadata = SettingsMetadata()) :
+    HasMetadata
